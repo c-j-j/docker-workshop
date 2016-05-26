fd00a38f6f2c2842bc9bf3206491490c7390fb5b
Chris Jordan <cjordan@8thlight.com>
2016-05-26 08:33:01 +0100

    Removing answers.

--- step_three/lib/app.rb
+++ step_three/lib/app.rb
@@ -4,7 +4,7 @@ require "redis"
 set :bind, '0.0.0.0'
 
 get '/' do
-  redis = Redis.new(:host => "db")
+  redis = Redis.new(:host => "CHANGE_ME")
 
   next_count = get_count(redis) + 1
   set_next_count(redis, next_count)
